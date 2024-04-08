a5e7fe04c3239815c4f699176258207282adf728
Braz Neto <jbon@cesar.school>
2024-04-08 10:08:47 -0300

    Add produtos no carrinho + quantidade de produtos no carrinho

--- Itech/cart/cart.py
+++ Itech/cart/cart.py
@@ -1,3 +1,5 @@
+from store.models import Product
+
 class Cart():
     def __init__(self,request):
         self.session = request.session
@@ -7,10 +9,24 @@ class Cart():
             cart = self.session['session_key'] = {}
 
         self.cart =cart
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
         if product_id  in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
+            #self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
         self.session.modified = True
+
+    def __len__(self):
+        return len(self.cart)
+    
+    def get_prods(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        return products
+    
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
