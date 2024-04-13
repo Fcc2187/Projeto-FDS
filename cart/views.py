565d5335155d3ecbe6fcead8a8c5818607b0bca6
Rodrigo Torres <rtmr@cesar.school>
2024-04-13 14:40:30 -0300

    precisa inspecionar pagina

--- cart/views.py
+++ cart/views.py
@@ -25,4 +25,12 @@ def cart_delete(request):
     pass
 
 def cart_update(request):  
-    pass
+    cart= Cart(request)
+    if request.POST.get('action')=='post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        cart.update (product=product_id, quantity= product_qty)
+
+        response = JsonResponse({'qty':product_qty})
+        return response
+        #return redirect ('cart_summary')
