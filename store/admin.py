c82a0ab3f19484072ca93ef3e57435ba540a5d2e
lfelipearruda <lfpa@cesar.school>
2024-04-22 11:05:30 -0300

    atualizar senha e extender perfil do usuario

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from . import models
+from django.contrib.auth.models import User
 
 admin.site.register(models.Category)
 
@@ -9,3 +10,16 @@ admin.site.register(models.Product)
 
 admin.site.register(models.Order)
 
+admin.site.register(models.Profile)
+
+class ProfileInline(admin.StackedInline):
+    model = models.Profile 
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ["username", "first_name", "last_name", "email" ]
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+
+admin.site.register(User, UserAdmin)
--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from .forms import SignUpForm, UpdateUserForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
 from .models import Product, Category
 
 def category_summary(request):
@@ -74,6 +74,29 @@ def register_user(request):
 	else:
 		return render(request, 'register.html', {'form':form})
 
+def update_password(request):
+    if request.user.is_authenticated:
+        current_user = request.user
+     
+        if request.method == 'POST':
+            form = ChangePasswordForm(current_user, request.POST)
+            if form.is_valid():
+                 form.save()
+                 messages.success(request, "Sua senha foi atualizada...")
+                 login(request, current_user)
+                 return redirect('update_user')
+            else:
+                for error in list(form.errors.values()):
+                     messages.error(request, error)
+                     return redirect('update_password')
+
+        else:
+            form = ChangePasswordForm(current_user)
+            return render(request, "update_password.html", {'form':form})
+    else:
+        messages.success(request, "Senha atualizada com sucesso!")
+        return redirect('home')
+    
 def update_user(request):
     if request.user.is_authenticated:
         current_user = User.objects.get(id=request.user.id)
