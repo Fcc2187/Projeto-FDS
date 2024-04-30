c1ec4ba93ee046198057e3592de070fb01610461
unknown <fcc3@cesar.school>
2024-04-30 20:32:06 -0300

    validando a historia de adicionar ao carrinho

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse,HttpResponse
 from django.contrib import messages
+
 
 def cart_summary(request):
     cart = Cart(request)
@@ -12,17 +13,20 @@ def cart_summary(request):
     return render(request,'cart_summary.html', {"cart_products":cart_products, "quantities":quantities, "totals":totals})
 
 def cart_add(request):
-    cart = Cart(request)
-    if request.POST.get('action')=='post':
-        product_id = int(request.POST.get('product_id'))
-        product_qty = int(request.POST.get('product_qty'))
-        product =  get_object_or_404(Product,id=product_id)
-        cart.add(product=product, quantity=product_qty)
-        cart_quantity = cart.__len__()
-        # response = JsonResponse({"Product name: ": product.name})
-        response = JsonResponse({'qty': cart_quantity})
-        messages.success(request, ("Produto adicionado com sucesso."))
-        return response
+        cart = Cart(request)
+        if request.POST.get('action')=='post':
+            product_id = int(request.POST.get('product_id'))
+            product_qty = int(request.POST.get('product_qty'))
+            product =  get_object_or_404(Product,id=product_id)
+            cart.add(product=product, quantity=product_qty)
+            cart_quantity = cart.__len__()
+            # response = JsonResponse({"Product name: ": product.name})
+            response = JsonResponse({'qty': cart_quantity})
+            messages.success(request, ("Produto adicionado com sucesso."))
+            return response
+        else:
+            messages.success(request, "Você precisa estar logado para adicionar esse produto.")
+            return redirect('home')
     
 def cart_delete(request):
     cart = Cart(request)
