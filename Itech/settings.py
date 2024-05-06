29bda0fb174f4dda91e455cee49d5c6a8525f318
Fcc2187 <142419397+Fcc2187@users.noreply.github.com>
2024-05-06 10:33:38 -0300

    Update settings.py

--- Itech/settings.py
+++ Itech/settings.py
@@ -99,14 +99,6 @@ WSGI_APPLICATION = 'Itech.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
